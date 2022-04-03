521932ed956bfefc6b86dee6dff7da3c91adc86b
alanndz <alanndz7@gmail.com>
2022-04-03 13:33:14 +0000

    chime: Move to chime-libperfmgr
    Signed-off-by: alanndz <alanndz7@gmail.com>
    Change-Id: I7b94875fdba8ea6ccaa6b558859bfafd4d602150
    Signed-off-by: frost. (frostg-012) <frost@nusantararom.org>
    Signed-off-by: Raya Mahardika (rktdnt.) <rktdnt@proton.me>

--- device.mk
+++ device.mk
@@ -443,7 +443,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi-libperfmgr \
+    android.hardware.power-service.chime-libperfmgr \
     libqti-perfd-client
 
 # Properties
