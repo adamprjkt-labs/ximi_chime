c15c1d7f969ce132e7ad8dbad3a42c17d5a492fc
ëin <michelsmithe2@gmail.com>
2022-09-20 16:49:19 +0700

    chime: update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,10 +2,13 @@ echo 'auto cloning vt,kt'
 
 # vendor tree
 echo 'cloning vt'
-git clone -b thirteen https://github.com/putraaxyzo/vendor_xiaomi_chime vendor/xiaomi/chime
+git clone -b thirteen https://gitlab.com/frstprjkt/vendor_xiaomi_chime vendor/xiaomi/chime
 
 # kernel tree
 echo 'cloning kt'
 git clone https://github.com/frstprjkt/kernel_xiaomi_chime-anya kernel/xiaomi/chime
 
-echo 'delete vendorsetup.sh from device tree once this is done'
+# deleting vendorsetup.sh
+rm -rf device/xiaomi/chime/vendorsetup.sh
+
+echo 'all done enjoys:)'
