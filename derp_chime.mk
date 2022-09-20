fcf099ddc663019c3407f83e1636b6720bf1f7b6
putraaxyzo <michelsmithe2@gmail.com>
2022-09-20 16:09:09 +0700

    chime: init DerpFest

--- derp_chime.mk
+++ derp_chime.mk
@@ -11,17 +11,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from chime device
 $(call inherit-product, device/xiaomi/chime/device.mk)
 
-# Inherit some common Eternity-OS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common DerpFest-AOSP stuff.
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_PIXEL_CHARGER := true
 
-# Eternity Stuff.
-ETERNITY_MAINTAINER := putrazxyo13
-CUSTOM_BUILD_TYPE := OFFICIAL
-
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_chime
+PRODUCT_NAME := derp_chime
 PRODUCT_DEVICE := chime
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := SM6115
