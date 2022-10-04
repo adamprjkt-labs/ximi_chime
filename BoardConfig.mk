8cf10bc414dd67ad9da26f9fa2183865d4e33653
Edwin Moquete <edwinmmoquete@gmail.com>
2022-10-04 22:14:39 +0000

    chime: Switch to legacy sepolicy_vndr
    Signed-off-by: anuragbhaumik <bhaumik463@gmail.com>
    Signed-off-by: Raya Mahardika (rktdnt.) <rktdnt@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -184,8 +184,8 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 ENABLE_VENDOR_RIL_SERVICE := true
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
-# SELinux
-include device/qcom/sepolicy_vndr/SEPolicy.mk
+# Sepolicy
+include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
