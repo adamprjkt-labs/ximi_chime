66010b4935ca8023c8a3fe5b74c3ee51671a5ca4
SebaUbuntu <barezzisebastiano@gmail.com>
2022-10-11 15:02:53 +0700

    chime: Build default health HIDL recovery impl Change-Id: Iaa6b7adcdef73f2485df77cf5df61b34d236eece

--- device.mk
+++ device.mk
@@ -304,7 +304,8 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl
 endif
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-service
+    android.hardware.health@2.1-service \
+    android.hardware.health@2.1-impl.recovery
 
 # HIDL
 PRODUCT_PACKAGES += \
