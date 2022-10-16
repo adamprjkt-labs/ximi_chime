2af693d627d79119870831b870c5a13396c6096a
imannig <zahranansaria@gmail.com>
2022-10-17 02:53:10 +0700

    chime: vendorsetup: auto clone legacy sepolicy vndr Signed-off-by: imannig <zahranansaria@gmail.com

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,6 +8,11 @@ git clone -b thirteen https://gitlab.com/frstprjkt/vendor_xiaomi_chime vendor/xi
 echo 'cloning kt'
 git clone https://github.com/frstprjkt/kernel_xiaomi_chime-anya kernel/xiaomi/chime
 
+# Sepolicy
+rm -rf vendor/qcom/sepolicy_vndr-legacy-um
+echo 'cloning sepolicy'
+git clone -b tiramisu https://github.com/Evolution-X/device_qcom_sepolicy_vndr-legacy-um vendor/qcom/sepolicy_vndr-legacy-um
+
 # deleting vendorsetup.sh
 rm -rf device/xiaomi/chime/vendorsetup.sh
 
